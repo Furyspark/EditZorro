3ab8ec129600e2927a0ac5bb46a511d0424be8a6
Tijmen Bok <mikauschekzen@gmail.com>
2016-09-05 07:02:03 +0200

    add: compiled script and output directory to gitignore

--- static/js/extended.js
+++ static/js/extended.js
@@ -1,15 +1,25 @@
 var ipcRenderer = require("electron").ipcRenderer;
 
 ipcRenderer.on("extended-params", function(event, args) {
-  console.log(args);
+  Core.start(args);
 });
 
 function Core() {
   console.log("This is a static class.");
 }
 
-Core.start = function() {
-  console.log("Ni hao!");
+Core.start = function(args) {
+  this.fillActionList();
+}
+
+Core.fillActionList = function() {
+  
+}
+
+Core.addToPress = function() {
+}
+
+Core.addToRelease = function() {
 }
 
 window.addEventListener("load", function() {
